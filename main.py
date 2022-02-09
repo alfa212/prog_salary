69244ef0ebea6e936c1597603f8320c2836ee797
alfa212 <angento@gmail.com>
2022-02-09 17:01:25 +0300

    code has been grouped into functions

--- main.py
+++ main.py
@@ -17,6 +17,62 @@ def predict_rub_salary(salary_currency, salary_from, salary_to):
         return (salary_from or 0) * 1.2 + (salary_to or 0) * 0.8
 
 
+def make_hh_table(vacancies, language):
+    results_table = []
+    vacancies_hh_processed = 0
+    hh_salary_sum = 0
+
+    for vacancy in vacancies[0]:
+        if not vacancy['salary']:
+            continue
+
+        vacancy_salary = predict_rub_salary(vacancy['salary']['currency'],
+                                            vacancy['salary']['from'],
+                                            vacancy['salary']['to'])
+
+        if not vacancy_salary:
+            continue
+
+        hh_salary_sum += int(vacancy_salary)
+        vacancies_hh_processed += 1
+
+    results_table += [[language,
+                  vacancies[1],
+                  vacancies_hh_processed,
+                  int(hh_salary_sum / (vacancies_hh_processed or 1))
+                  ]]
+
+    return results_table
+
+
+def make_sj_table(vacancies, language):
+    results_table = []
+    vacancies_sj_processed = 0
+    sj_salary_sum = 0
+
+    for vacancy in vacancies[0]:
+        if not vacancy["payment_from"] and not vacancy["payment_to"]:
+            continue
+
+        vacancy_salary = predict_rub_salary(vacancy["currency"],
+                                            vacancy["payment_from"],
+                                            vacancy["payment_to"])
+
+        if not vacancy_salary:
+            continue
+
+        sj_salary_sum += int(vacancy_salary)
+        vacancies_sj_processed += 1
+
+    results_table += [[language,
+                  vacancies[1],
+                  vacancies_sj_processed,
+                  int(sj_salary_sum / (vacancies_sj_processed or 1))
+                  ]]
+
+    return results_table
+
+
 if __name__ == '__main__':
     load_dotenv()
 
@@ -29,6 +85,7 @@ if __name__ == '__main__':
     pop_languages = os.getenv('POP_LANGUAGES').split(",")
 
     table_heading = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
+
     hh_table = []
     sj_table = []
 
@@ -39,51 +96,8 @@ if __name__ == '__main__':
         found_hh_vacancies = fetch_hh.fetch_hh_vacancies_info(hh_speciality, hh_search_city, hh_search_period, language)
         found_sj_vacancies = fetch_sj.fetch_sj_vacancies_info(sj_headers, language)
 
-        vacancies_hh_processed = 0
-        vacancies_sj_processed = 0
-
-        hh_salary_sum = 0
-        sj_salary_sum = 0
-
-        for vacancy in found_hh_vacancies[0]:
-            if not vacancy['salary']:
-                continue
-
-            vacancy_salary = predict_rub_salary(vacancy['salary']['currency'],
-                                                vacancy['salary']['from'],
-                                                vacancy['salary']['to'])
-
-            if not vacancy_salary:
-                continue
-
-            hh_salary_sum += int(vacancy_salary)
-            vacancies_hh_processed += 1
-
-        for vacancy in found_sj_vacancies[0]:
-            if not vacancy["payment_from"] and not vacancy["payment_to"]:
-                continue
-
-            vacancy_salary = predict_rub_salary(vacancy["currency"],
-                                                vacancy["payment_from"],
-                                                vacancy["payment_to"])
-
-            if not vacancy_salary:
-                continue
-
-            sj_salary_sum += int(vacancy_salary)
-            vacancies_sj_processed += 1
-
-        hh_table += [[language,
-                      found_hh_vacancies[1],
-                      vacancies_hh_processed,
-                      int(hh_salary_sum / (vacancies_hh_processed or 1))
-                      ]]
-
-        sj_table += [[language,
-                      found_sj_vacancies[1],
-                      vacancies_sj_processed,
-                      int(sj_salary_sum / (vacancies_sj_processed or 1))
-                      ]]
+        hh_table += make_hh_table(found_hh_vacancies, language)
+        sj_table += make_sj_table(found_sj_vacancies, language)
 
     output_hh_table = AsciiTable(table_heading + hh_table, 'HeadHunter Moscow')
     output_sj_table = AsciiTable(table_heading + sj_table, 'SuperJob Moscow')
